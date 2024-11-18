066daf340f8b1799da5d3c6ecb1d753881c35cc3
Darryl de Jong <darryldjong@gmail.com>
2024-11-18 23:56:42 +0100

    Update Autoclicker.py

--- Autoclicker.py
+++ Autoclicker.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 import threading
 import time
 import os
+import keyboard
 
 class AutoClickerApp:
     def __init__(self, root):
@@ -21,13 +22,18 @@ class AutoClickerApp:
         
         self.setup_ui()
         
-        # Keybindings
-        self.root.bind("<F1>", self.start_clicking_key)  # F1 voor Start
-        self.root.bind("<F6>", self.start_clicking_key)  # F6 voor Start
-        self.root.bind("<F2>", self.stop_clicking_key)   # F2 voor Stop
-        self.root.bind("<F7>", self.stop_clicking_key)   # F7 voor Stop
+        threading.Thread(target=self.setup_keybindings, daemon=True).start()
+
+    def setup_keybindings(self):
+        keyboard.add_hotkey('F1', self.start_clicking)
+        keyboard.add_hotkey('F6', self.start_clicking)
+        
+        keyboard.add_hotkey('F2', self.stop_clicking)
+        keyboard.add_hotkey('F7', self.stop_clicking)
 
     def setup_ui(self):
+        vcmd = (self.root.register(self.validate_interval), '%d', '%P')
+        
         ttk.Label(self.root, text="Value").grid(row=0, column=0, padx=5, pady=5, sticky="w")
         self.interval_unit_combo = ttk.Combobox(self.root, values=["Min", "Sec", "Ms"], width=10, state="readonly")
         self.interval_unit_combo.current(2)
@@ -35,8 +41,8 @@ class AutoClickerApp:
         self.interval_unit_combo.bind("<<ComboboxSelected>>", self.on_unit_change)
         
         ttk.Label(self.root, text="Time").grid(row=0, column=2, padx=5, pady=5, sticky="w")
-        self.interval_entry = ttk.Entry(self.root, width=10, justify="center")
-        self.interval_entry.insert(0, str(self.interval_value))
+        self.interval_entry = ttk.Entry(self.root, width=10, justify="center", validate="key", validatecommand=vcmd)
+        self.interval_entry.insert(0, str(self.interval_value))  
         self.interval_entry.grid(row=0, column=3, padx=5)
 
         ttk.Label(self.root, text="Click").grid(row=1, column=0, padx=5, pady=5, sticky="w")
@@ -54,6 +60,11 @@ class AutoClickerApp:
         self.stop_button = ttk.Button(self.root, text="Stop F2 or F7", command=self.stop_clicking)
         self.stop_button.grid(row=2, column=2, columnspan=2, pady=10, padx=5, sticky="ew")
 
+    def validate_interval(self, action, value_if_allowed):
+        if action == '1':  
+            return value_if_allowed.isdigit()
+        return True
+
     def on_unit_change(self, event):
         if self.interval_unit_combo.get() == "Ms":
             self.interval_value = 500  # Minimaal 500 ms
@@ -70,31 +81,19 @@ class AutoClickerApp:
         if interval_input.isdigit():
             self.interval_value = int(interval_input)
         else:
-            self.on_unit_change(None)
-            self.interval_entry.delete(0, 'end')
-            self.interval_entry.insert(0, str(self.interval_value))
+            return 
         
         self.interval_unit = self.interval_unit_combo.get()
 
-        if self.interval_unit == "Ms":
-            if self.interval_value < 500:
-                self.interval_value = 500
-                self.interval_entry.delete(0, 'end')
-                self.interval_entry.insert(0, str(self.interval_value))
-            elif self.interval_value > 10000:
-                self.interval_value = 10000
-                self.interval_entry.delete(0, 'end')
-                self.interval_entry.insert(0, str(self.interval_value))
-        
+        if self.interval_unit == "Ms" and self.interval_value < 500:
+            self.interval_value = 500
         elif self.interval_unit == "Sec" and self.interval_value > 600:
             self.interval_value = 600
-            self.interval_entry.delete(0, 'end')
-            self.interval_entry.insert(0, str(self.interval_value))
-        
         elif self.interval_unit == "Min" and self.interval_value > 60:
             self.interval_value = 60
-            self.interval_entry.delete(0, 'end')
-            self.interval_entry.insert(0, str(self.interval_value))
+
+        self.interval_entry.delete(0, 'end')
+        self.interval_entry.insert(0, str(self.interval_value))
 
         self.click_type = self.click_type_combo.get()
         self.button_type = self.button_type_combo.get()
@@ -102,11 +101,8 @@ class AutoClickerApp:
         
         threading.Thread(target=self.perform_clicks, daemon=True).start()
 
-    def start_clicking_key(self, event=None):
-        self.start_clicking()
-
-    def stop_clicking_key(self, event=None):
-        self.stop_clicking()
+    def stop_clicking(self):
+        self.is_running = False
 
     def perform_clicks(self):
         if self.interval_unit == "Min":
@@ -123,9 +119,6 @@ class AutoClickerApp:
                 pyautogui.doubleClick(button=self.button_type.lower())
             
             time.sleep(total_delay)
-
-    def stop_clicking(self):
-        self.is_running = False
 
 if __name__ == "__main__":
     root = tk.Tk()
